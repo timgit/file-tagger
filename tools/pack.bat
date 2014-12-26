e09caf6b4b90395f998703afa5aa6b1caace2383
Tim Jones <tjneweyes@gmail.com>
2014-12-25 23:07:33 -0600

    NuGet package spec

---
+++ tools/pack.bat
@@ -0,0 +1,28 @@
+@echo off
+IF "%~1"=="" goto error
+
+SETLOCAL
+
+SET VERSION=%1
+SET PUSH=%2
+SET NUGET=..\src\.nuget\nuget.exe
+
+SET PACKAGE_SPEC=..\src\FileTagger\FileTagger.nuspec
+SET PACKAGE_NAME=FileTagger
+
+%NUGET% pack %PACKAGE_SPEC% -Version %VERSION%
+
+IF "%PUSH%"=="-push" goto push
+
+goto end
+
+:push
+%NUGET% push %PACKAGE_NAME%.%VERSION%.nupkg
+goto end
+
+:error
+echo ** Error: Missing version parameter.  
+echo Usage: pack VERSION [-push]
+echo For example: pack 1.0.3-alpha -push
+
+:end
